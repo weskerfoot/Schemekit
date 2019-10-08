52544799890f7681442babe108cbff684227c417
Wesley Kerfoot <wjak56@gmail.com>
2019-10-08 00:12:13 -0400

    Update name of program

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-gcc $(pkg-config --cflags --libs guile-2.2 gtk+-3.0 webkit2gtk-4.0) browser.c
+gcc $(pkg-config --cflags --libs guile-2.2 gtk+-3.0 webkit2gtk-4.0) browser.c -o schemekit
